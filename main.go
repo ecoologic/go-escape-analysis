d4305c23f7abb502b3b6e233bc8626e47bb860ba
Erik Trapin (ecoologic) <erik.trapin@zendesk.com>
2023-05-05 16:15:18 +1000

    logGC

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 	"runtime"
@@ -35,24 +36,38 @@ func bigData() []byte {
 	return data
 }
 
+func logGC(data *[]byte, name string) {
+	runtime.SetFinalizer(data, func(interface{}) {
+		fmt.Printf("%s garbage collected\n", name)
+	})
+}
+
 func run() {
 	badData := getBadData()
+	fmt.Println("1\n")
 	runtime.GC()
+	fmt.Println("2\n")
 	println("getBadData:", *badData, len(*badData))
 
 	var goodData []byte
+	fmt.Println("3\n")
 	getGoodData(&goodData)
+	fmt.Println("4\n")
 	runtime.GC()
 	println("getGoodData", goodData, len(goodData))
+	runtime.GC()
+	fmt.Println("5\n")
 }
 
 //go:noinline
 func getBadData() *[]byte {
 	data := bigData() // :52:2: data escapes to heap
+	logGC(&data, "badData")
 	return &data
 }
 
 //go:noinline
 func getGoodData(data *[]byte) { // :57:18: data does not escape
 	*data = bigData()
+	logGC(data, "goodData")
 }
